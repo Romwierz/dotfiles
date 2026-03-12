8b7ab9e394405fb8fc5df8bcbbecb06270c958b5
Romwierz <miczu47@gmail.com>
2026-03-12 11:36:26 +0100

    nvim: modify cursor opts

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -77,7 +77,7 @@ vim.opt.modifiable = true                          -- Allow buffer modifications
 vim.opt.encoding = "UTF-8"                         -- Set encoding
 vim.g.loaded_netrw = 1; vim.g.loaded_netrw = 1     -- Disable netrw to use other file explorer
 -- Cursor settings
---vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.guicursor="n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor,a:blinkon500-blinkoff300,n:blinkon0"
 
 -- Command-line completion
 vim.opt.wildmenu = true
